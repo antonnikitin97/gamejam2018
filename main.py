245cc8e4fedf784f1f3c2fa1bdd234343c840f35
Anton <antonnikitin@live.co.uk>
2018-01-26 19:16:39 +0000

    add skeleton code

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello world!")
+import sys
+import pygame
+
+
+class Game():
+
+    def __init__(self):
+        pygame.init()
+        self.done = False
+        self.dimensionX = 640
+        self.dimensionY = 480
+        self.screen_dimensions = tuple([self.dimensionX, self.dimensionY])
+        self.screen = pygame.display.set_mode(self.screen_dimensions)
+
+    def main_loop(self):
+        while not self.done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+
+            pygame.display.flip()
+
+
+game = Game()
+game.main_loop()
