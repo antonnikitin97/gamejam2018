163f3f27b8a456be469a243c0b86fe9df20e30e5
Mark Garnett <thecommunistduck@hotmail.co.uk>
2018-01-27 01:31:48 +0000

    added pitchshifted birb noises

--- house_generator.py
+++ house_generator.py
@@ -4,6 +4,9 @@ import random
 from pyknon.genmidi import Midi
 from pyknon.music import NoteSeq
 import pygame
+import time, numpy, pygame.mixer, pygame.sndarray
+from scipy.signal import resample
+import numpy as np
 
 class Point:
     def __init__(self,x_init,y_init):
@@ -81,8 +84,8 @@ def generate_IP_message(len, to):
 def music_seq(length):
 	#start on a random note
 	#A-G = 0-7
-	note_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-	start = random.randint(0, 7)
+	note_names = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+	start = random.randint(0, 12)
 	start_offset = 0
 	
 	curr_offset = start_offset
@@ -93,17 +96,14 @@ def music_seq(length):
 			if len(notes) > 1 and (notes[-1] == notes[-2]) and step == 0:
 				continue
 			print(step)
-			if abs(curr_offset + step) > 7:
+			if abs(curr_offset + step) > 12:
 				continue
 			else:
 				curr_offset = curr_offset + step
 				notes.append(curr_offset + start)
 				break
 	#notes = [random.randint(-6, 7) for _ in range(length)]
-	return [note_names[n % 7]+str(3 if n < 0 else (5 if n == 7 else 4)) for n in notes]
-	
-
-
+	return [note_names[n % 12]+str(3 if n < 0 else (5 if n == 12 else 4)) for n in notes]
 
 if __name__ == "__main__":
 	#generate_house_locations()
@@ -121,6 +121,7 @@ if __name__ == "__main__":
 	# optional volume 0 to 1.0
 	music_file = "demo.mid"
 	pygame.mixer.music.set_volume(0.8)
+	'''
 	try:
 	    play_music(music_file)
 	except KeyboardInterrupt:
@@ -129,3 +130,22 @@ if __name__ == "__main__":
 	    pygame.mixer.music.fadeout(1000)
 	    pygame.mixer.music.stop()
 	    raise SystemExit
+	'''
+	# choose a file and make a sound object
+	sound_file = "bir2.wav"
+	sound = pygame.mixer.Sound(sound_file)
+
+	# load the sound into an array
+	snd_array = pygame.sndarray.array(sound)
+
+	# resample. args: (target array, ratio, mode), outputs ratio * target array.
+	# this outputs a bunch of garbage and I don't know why.
+	sounds = []
+	for i in range(12):
+			snd_resample = resample(snd_array, int(42404*(2**(i/12)))).astype(np.int16, order='C')
+			sounds = sounds + [pygame.sndarray.make_sound(snd_resample)]
+
+	# take the resampled array, make it an object and stop playing after 2 seconds.
+	for snd in sounds:
+			snd.play()
+			time.sleep(2)
