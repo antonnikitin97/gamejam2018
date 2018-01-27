4a8b41b3afa4e23586d2f44b0b8d0f07eefad743
Charlie <cst1g16@soton.ac.uk>
2018-01-27 00:06:13 +0000

    Add pygame initialisation to state machine

--- StateMachine.py
+++ StateMachine.py
@@ -1,11 +1,13 @@
 import pygame
-import main
+import main, menu
+pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
 
+menustate = menu.Menu(screen)
 mapstate = main.Game(screen)
 
-state = mapstate
+state = menustate
 
 while True:
     state = state.main_loop()
