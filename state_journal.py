303f8c0fa7125ce475eff3ab0573998a5dab3899
Charlie <cst1g16@soton.ac.uk>
2018-01-28 10:56:19 +0000

    acknowledged Anton's awesomness - #InspiringCommits

--- state_journal.py
+++ state_journal.py
@@ -2,9 +2,10 @@ import pygame
 from pygame.locals import *
 
 class Book:
-    def __init__(self, screen, options, statein, map, houses):
+    def __init__(self, screen, options, statein, map, houses, player):
         self.done = False
         self.screen = screen
+        self.player = player
         self.dimensionX = screen.get_width()
         self.dimensionY = screen.get_height()
         self.textfont = pygame.font.Font('Assets/OpenSans-Regular.ttf', 30)
@@ -24,17 +25,17 @@ class Book:
         self.page.fill((255, 255, 255))
         pygame.draw.line(self.page, (0, 0, 0), (0, 0), (0, self.dimensionY), 4)
         pygame.draw.line(self.page, (0, 0, 0), (self.dimensionX/2 - 2, 0), (self.dimensionX/2 - 2, self.dimensionY), 4)
-        self.symbols = [pygame.transform.scale(pygame.image.load_extended('Assets/symbols/{}.png'.format(i)),
-                                               (100, 100)).convert_alpha()
-                        for i in range(1, 21)]
+        self.symbols = [pygame.image.load_extended('Assets/symbols/{}.png'.format(i)) for i in range(1, 21)]
+        self.bigsymbols = [pygame.transform.scale(symbol, (100, 100)).convert_alpha() for symbol in self.symbols]
+        self.smallsymbols = [pygame.transform.scale(symbol, (50, 50)).convert_alpha() for symbol in self.symbols]
         titles = ["PostCodes 1", "PostCodes 2", "Island Map West", "Island Map East", "Current Jobs"]
         self.pages = [self.page.copy() for i in range(len(titles))]
         for i in range(5):
-            self.pages[0].blit(self.symbols[0], (10, 100 + i * 110))
-            self.pages[0].blit(self.symbols[int(i % 5)], (110, 100 + i * 110))
+            self.pages[0].blit(self.bigsymbols[0], (10, 100 + i * 110))
+            self.pages[0].blit(self.bigsymbols[int(i % 5)], (110, 100 + i * 110))
             self.pages[0].blit(self.textfont.render("House " + str(i), True, (0, 0, 0)), (210, 100 + i * 110))
-            self.pages[1].blit(self.symbols[1], (10, 100 + i * 110))
-            self.pages[1].blit(self.symbols[int((i + 5) % 5)], (110, 100 + i * 110))
+            self.pages[1].blit(self.bigsymbols[1], (10, 100 + i * 110))
+            self.pages[1].blit(self.bigsymbols[int((i + 5) % 5)], (110, 100 + i * 110))
             self.pages[1].blit(self.textfont.render("House " + str(i + 5), True, (0, 0, 0)), (210, 100 + i * 110))
         westmap = pygame.transform.scale(self.map.subsurface((0, 0, self.map.get_width()/2, self.map.get_height())),
                                                   (int(self.map.get_width()/20), int(self.map.get_height()/10))).convert_alpha()
@@ -42,6 +43,14 @@ class Book:
                                                   (int(self.map.get_width() / 20), int(self.map.get_height()/10))).convert_alpha()
         self.pages[2].blit(westmap, (self.dimensionX/2 - westmap.get_width() - 5, 50))
         self.pages[3].blit(eastmap, (5, 50))
+        print(self.player.current_transmissions)
+        pygame.draw.line(self.pages[4], (0, 0, 0), (130, 0), (130, self.dimensionY))
+        for i, transmission in enumerate(self.player.current_transmissions):
+            id = transmission[0]
+            self.pages[4].blit(self.smallsymbols[int((id - (id % 5)) / 5)], (10, 150 + i * 60))
+            self.pages[4].blit(self.smallsymbols[int(id % 5)], (65, 150 + i * 60))
+            for j, no in enumerate(transmission[1]):
+                self.pages[4].blit(self.smallsymbols[no], (150 + j * 55, 200 + i * 60))
         for i in range(len(self.pages)):
             self.pages[i].blit(self.textfont.render(titles[i], True, (0, 0, 0), (255, 255, 255)), (10, 10))
             self.pages[i].blit(self.textfont.render("Pg. " + str(i + 1), True, (0, 0, 0), (255, 255, 255)), (10, self.dimensionY - 45))
@@ -61,11 +70,14 @@ class Book:
                 if e.type == QUIT:
                     quit()
                 if e.type == KEYDOWN:
-                    if e.key in [K_ESCAPE, K_s]:
+                    if e.key in [K_ESCAPE, K_j]:
                         self.leavejournal()
-                    if e.key in [K_LEFT, K_a]:
+                    if e.key == K_LEFT:
                         self.pageselector -= 1
-                    if e.key in [K_RIGHT, K_d]:
+                    if e.key == K_RIGHT:
                         self.pageselector += 1
                     self.pageselector = min(max(self.pageselector, 0), len(self.pages) - 2)
         return self.nextstate
+
+"""I think Anton is the best and he should be given many doggos
+~ S T A Y  I N S P I R E D ~"""
--- state_main.py
+++ state_main.py
@@ -165,14 +165,13 @@ class Game:
         pygame.time.set_timer(self.transmission_event, self.transmission_time * 1000)
         self.start = 0
         self.paused = False
-        self.journalstate = state_journal.Book(self.screen, self.options, self, self.map, self.house_list)
         
     def rotatePoint(self, centerPoint, point, angle):
         """Rotates a point around another centerPoint. Angle is in degrees.
         Rotation is counter-clockwise"""
         angle = math.radians(angle)
         temp_point = point[0] - centerPoint[0] , point[1] - centerPoint[1]
-        temp_point = ( temp_point[0]*math.cos(angle)-temp_point[1]*math.sin(angle) , temp_point[0]*math.sin(angle)+temp_point[1]*math.cos(angle))
+        temp_point = ( temp_point[0]*math.cos(angle)-temp_point[1]*math.sin(angle), temp_point[0]*math.sin(angle)+temp_point[1]*math.cos(angle))
         temp_point = temp_point[0]+centerPoint[0] , temp_point[1]+centerPoint[1]
         return temp_point
 
@@ -236,7 +235,7 @@ class Game:
     
     def enterjournal(self):
         self.done = True
-        self.nextstate = self.journalstate
+        self.nextstate = state_journal.Book(self.screen, self.options, self, self.map, self.house_list, self.player)
         self.pause()
         
     def pause(self):
